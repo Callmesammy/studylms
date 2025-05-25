f58b51260339faa7fb9cd87d14b5d157b23084cb
Callmesammy <callmesammy@yahoo.com>
2025-05-25 09:51:19 +0100

    update

--- app/(root)/_componets/header.tsx
+++ app/(root)/_componets/header.tsx
@@ -5,13 +5,33 @@ import type { Metadata } from "next";
 import { Pacifico } from "next/font/google";
 import { useEffect } from "react";
 import gsap from "gsap";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const pacifico = Pacifico({    
      weight: '400',
     subsets: ["latin"],
 });
 
+const Headstcky=[
+    {
+        name: "Home", 
+        url: "/"
+    },
+    {
+        name: "Learning Companions", 
+        url: "/companions"
+    },
+
+    {
+        name: "My Journer", 
+        url: "/journer"
+    },
+
+]
 const Headers = () => {
+    const pathName = usePathname()
     const clicking =(url: string)=>{
         window.location.href = url
     }
@@ -48,13 +68,22 @@ const Headers = () => {
     )
     },[])
     return ( 
-        <div className="w-full h-[5rem] bg-white flex">
+        <div className="w-full h-[5rem] bg-white flex shadow-sm">
             <div className="w-full h-full items-center flex px-2 justify-between">
             <div onClick={()=>clicking("/")} className="flex gap-1 items-center cursor-pointer hover:scale-105">
             <Image src="/study.avif" alt="logo" width={50} height={50} className="rounded-md shadow press"/>
            <div className={pacifico.className}> <h1 className="twist">StusyLMS </h1> </div> 
             </div>
-            jkhj    
+            <div className="gap-3 flex">
+            {Headstcky.map((doc, keys)=>{
+                const isActive = pathName===doc.url || pathName.startsWith(`${doc.url}/`)
+                return(
+                    <Link href={doc.url} key={keys} className={cn("space-x-2 text-md font-semibold hover:text-blue-400 ", isActive && "text-blue-600 font-bold")} >
+                        {doc.name}
+                    </Link>
+                )
+            })}
+            </div>    
             </div> 
         </div>
      );
