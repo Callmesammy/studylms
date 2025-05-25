f69ba4b55b2811e2657dbbe26a32e35e443833c0
Callmesammy <callmesammy@yahoo.com>
2025-05-25 17:19:42 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,10 @@ import {  courses } from "./_componets";
 import { CiBookmark } from "react-icons/ci";
 import { IoTimerOutline } from "react-icons/io5";
 import { MdOutlineScience } from "react-icons/md";
+import { FaCalculator } from "react-icons/fa";
+import { PiChatsTeardropDuotone } from "react-icons/pi";
+import { MdOutlineComputer } from "react-icons/md";
+import { GiNotebook } from "react-icons/gi";
 
 
 
@@ -20,6 +24,55 @@ export const anotherCourse =[
   lunch: ()=> {}, 
   colour: "bg-purple-300",
   icon: <MdOutlineScience className="size-7 flex"/>,
+},
+
+{
+  id: 2,
+  course: "Maths", 
+  name: "Algebra: Equations", 
+  topic: "Solving Linear Equations", 
+  time: 20, 
+  duration: "mins durations",
+  bookmark: ()=> {}, 
+  lunch: ()=> {}, 
+  colour: "bg-yellow-300",
+  icon: <FaCalculator className="size-7 flex"/>,
+},
+{
+  id: 3,
+  course: "Language", 
+  name: "Luna, Your Gramma Guide", 
+  topic: "Mastering Tenses in English", 
+  time: 15, 
+  duration: "mins durations",
+  bookmark: ()=> {}, 
+  lunch: ()=> {}, 
+  colour: "bg-sky-300",
+  icon: <PiChatsTeardropDuotone className="size-7 flex"/>,
+},
+{
+  id: 4,
+  course: "Coding", 
+  name: "Codey, the Logic Hacker", 
+  topic: "Intro to If-Else Statements", 
+  time: 35, 
+  duration: "mins durations",
+  bookmark: ()=> {}, 
+  lunch: ()=> {}, 
+  colour: "bg-pink-300",
+  icon: <MdOutlineComputer className="size-7 flex"/>,
+},
+{
+  id: 5,
+  course: "History", 
+  name: "Memo, The Memo Keeper", 
+  topic: "World wars: Causes & Effects", 
+  time: 25, 
+  duration: "mins durations",
+  bookmark: ()=> {}, 
+  lunch: ()=> {}, 
+  colour: "bg-orange-300",
+  icon: <GiNotebook className="size-7 flex"/>,
 },
 
 
@@ -52,15 +105,17 @@ export default function Home() {
           </div>
         ))}
       </div>
-     <div className="w-full h-full flex gap-3  ">
-        <div className="border p-2 rounded-md w-full">
+      
+     <div className="grid lg:grid-cols-2 gap-3  ">
+        <div className="border p-2 rounded-md w-full ">
           <h1 className="text-md font-bold">Recently Completed Lessons</h1>
           <div className="w-full justify-between h-full   ">
            <span className="space-y-3 pt-3 w-full flex flex-col justify-between">
            <div className="flex w-full justify-between px-2">
            <h1 className="text-sm text-muted-foreground">Lessons</h1>
-           <span>
+           <span className="flex gap-4">
              <h1 className="text-sm text-muted-foreground">Subjects</h1>
+             <h1 className="text-sm text-muted-foreground hidden md:flex">Duration</h1>
 
            </span>
 
@@ -69,7 +124,7 @@ export default function Home() {
             {anotherCourse.map((dt)=>{
 
               return(
-                <Link href={`/course${dt.id}`} key={dt.id} className="w-full flex gap-2 hover:bg-secondary  ">
+                <Link href={`/course${dt.id}`} key={dt.id} className="w-full flex gap-2 hover:bg-sky-200  ">
                    <span className={`"flex p-3 rounded bg w-full" ${dt.colour}`}> {dt.icon} </span> 
                   <div className="w-full flex justify-between gap-10">
                     <span className="flex flex-col w-full ">
@@ -79,10 +134,15 @@ export default function Home() {
                   
 
                     </div>
-
+                    <span className="flex gap-2 w-full justify-end ">
                     <span className="p-2 border rounded-full items-center flex text-sm text-white bg-black font-semibold text-end">
                       {dt.course}
                     </span>
+                    <span className="p-2  hidden md:flex rounded-full items-center text-sm font-semibold text-end">
+                      {dt.time} mins
+                    </span>
+                    </span>
+                   
                 </Link>
               )
             })}
@@ -90,8 +150,12 @@ export default function Home() {
            
           </div>
         </div>
-      
+       <div className="w-full lg:w-[50rem] h-full bg-black rounded-md">
+
+        
+      </div>
       </div> 
+     
       </div> 
 
 
