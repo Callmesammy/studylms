0d5b15befd251f07acdf19bb1b3ec3ca7d71cd84
Callmesammy <callmesammy@yahoo.com>
2025-05-29 12:54:01 +0100

    update

---
+++ app/(root)/_componets/companion-card.tsx
@@ -0,0 +1,70 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { IoTimerOutline } from "react-icons/io5"
+import Link from "next/link"
+import { CiBookmark } from "react-icons/ci"
+
+interface cardProps {
+    id?: number,
+    course?: string,
+    name?: string,
+    topic?: string, 
+    time?: number,
+    duration?: string,
+    bookmark?: ()=> {}, 
+    lunch?: ()=> {}, 
+    colour?: string
+
+}
+export function AddCompannion({
+    id,
+    course,
+    name,
+    topic,
+    time,
+    duration,
+    bookmark,
+    lunch,
+    colour,
+
+}: cardProps) {
+  return (
+    <Card className={`"w-[350px] border border-black " ${colour}`}>
+     
+      <CardContent>
+      <div key={id} className={`"flex  rounded-lg px-3 " ${colour}`}>
+            <div className="justify-between w-full px-4 pt-6 flex ">
+              <span className="bg-black text-white text-sm p-2 rounded">{course}</span>
+            
+              <span onClick={bookmark}  className="bg-black text-white text-sm p-2 rounded">  <CiBookmark className="size-4"/>
+              </span>
+            </div>
+            <div className="flex space-y-3 px-3 pt-3 flex-col ">
+            <h1 className="font-bold text-xl">{name}</h1>
+            <h2 className=" text-md">Topic: {topic}</h2>
+            <div className="flex space-x-2 items-center">
+            <IoTimerOutline />
+            <h1 className="text-sm">{time} {duration}</h1>
+            </div>
+            </div>
+            <div className="pt-5">
+            </div>
+          </div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+                   <Link href={`/course${id}`} onClick={lunch} className=" px-4 w-full bg-red-500 p-2 flex items-center rounded-md text-center hover:bg-red-700 justify-center text-md font-semibold text-white">Launch Lesson</Link>
+
+      </CardFooter>
+    </Card>
+  )
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,6 +10,7 @@ import { PiChatsTeardropDuotone } from "react-icons/pi";
 import { MdOutlineComputer } from "react-icons/md";
 import { GiNotebook } from "react-icons/gi";
 import { GoPlus } from "react-icons/go";
+import { AddCompannion } from "./_componets/companion-card";
 
 
 
@@ -84,27 +85,7 @@ export default function Home() {
     <div className="flex w-full h-full flex-col ">
        <h1 className="text-2xl font-bold">Dashboard</h1>
  <div className="grid md:grid-cols-2 lg:grid-cols-3 w-full h- lg:h-[17rem] gap-2 p-4 ">
-        {courses.map((doc)=>(
-          <div key={doc.id} className={`"flex border border-black rounded-lg px-3 " ${doc.colour}`}>
-            <div className="justify-between w-full px-4 pt-6 flex ">
-              <span className="bg-black text-white text-sm p-2 rounded">{doc.course}</span>
-
-              <span onClick={doc.bookmark}  className="bg-black text-white text-sm p-2 rounded">  <CiBookmark className="size-4"/>
-              </span>
-            </div>
-            <div className="flex space-y-3 px-3 pt-3 flex-col ">
-            <h1 className="font-bold text-xl">{doc.name}</h1>
-            <h2 className=" text-md">Topic: {doc.topic}</h2>
-            <div className="flex space-x-2 items-center">
-            <IoTimerOutline />
-            <h1 className="text-sm">{doc.time} {doc.duration}</h1>
-            </div>
-            </div>
-            <div className="pt-5">
-            <Link href={`/course${doc.id}`} onClick={doc.lunch} className=" px-4 w-full bg-red-500 p-2 flex items-center rounded-md text-center hover:bg-red-700 justify-center text-md font-semibold text-white">Launch Lesson</Link>
-            </div>
-          </div>
-        ))}
+       <AddCompannion  id= {1} course ="Science" name= "Neura the Brainy Explorer" topic= "Neural Network of the Brain" time={45} duration= "mins durations" colour= "bg-purple-300"      /> 
       </div>
       
      <div className="grid md:grid-cols-2 gap-2 w-full h-full">
