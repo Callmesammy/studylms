e9540adf931b17f684245b6f74491500ddab3223
Callmesammy <callmesammy@yahoo.com>
2025-07-05 18:58:33 +0100

    savvr

--- app/(root)/course/actions.ts
+++ app/(root)/course/actions.ts
@@ -1,8 +1,8 @@
 "use server"
 import {  z } from "zod"
 import { formSchema } from "./page"
-import { db } from "@/drizzle/src";
 import { usersTable } from "@/drizzle/db/schema";
+import { db } from "@/drizzle/src";
 
 export default async function insertCompanion(formData: z.infer<typeof formSchema>){
  const result = await db.insert(usersTable).values({...formData, createdAt: new Date()}).returning()
