11c588447ea33e42494370eaa6503dd8ca9d5288
Callmesammy <callmesammy@yahoo.com>
2025-07-05 11:16:03 +0100

    save

--- app/(root)/companions/page.tsx
+++ app/(root)/companions/page.tsx
@@ -3,6 +3,7 @@ import FilterInput from "../_componets/filter-input";
 import { usersTable } from "@/drizzle/db/schema";
 import { db } from "@/drizzle/src";
 import { AddCompannion } from "../_componets/companion-card";
+import { Suspense } from "react";
 
 
 
@@ -14,8 +15,11 @@ const CompanionDocuments = async () => {
             <h1 className="font-semibold text-2xl text-pretty">Companion Library</h1>
 
             <div className="flex gap-4">
+              <Suspense fallback= {<p>Loading search...</p>}>
+
               <SearchHere/>
               <FilterInput/>
+              </Suspense>
 
             </div>
              <div className="grid h- md:grid-cols-2 lg:grid-cols-3 gap-2  w-full   ">
